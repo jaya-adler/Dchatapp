cee99f7bb087cba93bd674530e601fc769292bdc
jaya-adler <jayashankar9741@gmail.com>
2022-01-24 21:22:35 +0530

    changed server

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    MORALIS_APP_ID : "u27fVetIAV8m75C52ZVT1lroVEbV409LXZLO3HVM",
-    MORALIS_SERVER_URL : "https://t6maj3gcxmfm.usemoralis.com:2053/server",
+    MORALIS_APP_ID : "VeP9DDR6kfCl5S1lKGui0BSmGXnlJzxwWTMaLkOU",
+    MORALIS_SERVER_URL : "https://rr94i9euznxz.usemoralis.com:2053/server",
   },
   images: {
     domains: ['avatars.dicebear.com','ipfs.moralis.io'],
